5e47ea04cb8af2c8a060178a49d60484015cf9ff
GUENWWE <52918965+GUENWWE@users.noreply.github.com>
2022-04-28 21:42:42 +0700

    Add files via upload

---
+++ Python/recusive_binary_search.py
@@ -0,0 +1,29 @@
+import numbers
+from unittest import result
+
+
+def recusive_binary_search(list, target): # recusive binary search algorithm
+  if len(list) == 0:
+      return False
+  else:
+    midpoint = (len(list))//2 # get the midpoint of the list
+
+    if list[midpoint] == target: # the condition if the midpoint is equal to the target
+      return True
+    else:
+      if list[midpoint] < target: 
+        return recusive_binary_search(list[midpoint+1:], target) # return the recusive_binary_search function with midpoit + 1
+      else:
+        return recusive_binary_search(list[:midpoint],target) # return the recusive_binary_search function with midpoit - 1
+def cin(result):
+  print("Target found in index at: ", result)
+  
+numbers = [1,2,3,4,5,6,7,8,9] # create a list with variable numbers
+
+result = recusive_binary_search(numbers,12)
+cin(result)
+
+result = recusive_binary_search(numbers, 12)
+print(result)
+      
+
