0bc9226cba3e942af4d9a81a75f8619ff25e110d
GUENWWE <52918965+GUENWWE@users.noreply.github.com>
2022-04-28 21:01:07 +0700

    Add files via upload

---
+++ guess_Game.py
@@ -0,0 +1,74 @@
+from math import radians
+import numbers
+from random import randint
+from IPython.display import clear_output
+
+guessed = False
+number = randint(0,10)
+guesses = 0
+while not guessed:
+  ans = input("Try to guess the number from 0 to 10: ")
+  #tab to indent
+  guesses += 1
+  clear_output() #clear the outpuyt
+  
+  if int(ans) == number:
+          print("Congrat!!! You guess is correctly")
+          #tab twice to indent twice
+          print("It tooks you {} guess".format(guesses))
+          break #break to out the condition
+        
+  elif int(ans) < number:
+          print("The number is higher than you guess...")
+  elif int(ans) > number:
+          print("The number is lower than you guess...")
+  
+          
+            
+    
+  
+    
+      
+    
+    
+      
+        
+    
+      
+    
+
+
+    
+      
+        
+          
+    
+      
+        
+          
+    
+  
+  
+
+    
+    
+      
+        
+          
+            
+            
+    
+  
+        
+        
+  
+      
+      
+        
+        
+    
+        
+    
+  
+  
+    
