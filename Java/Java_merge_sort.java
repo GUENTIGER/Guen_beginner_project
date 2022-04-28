e6b78717f74a4b72617ed4ecb201a658dd1460a2
GUENTIGER <52918965+GUENTIGER@users.noreply.github.com>
2022-04-28 22:35:38 +0700

    Update Java_merge_sort.java

--- Java/Java_merge_sort.java
+++ Java/Java_merge_sort.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-public class Java_merge_sort { // Merge sort algorithm
+public class Java_merge_sort { 
     static List<Integer> mergeSort(List<Integer> list) { 
-        if (list.size() <= 1) { // if the list has only one element, return the list 
+        if (list.size() <= 1) { 
             return list;
         }
         int middleIndex = list.size() / 2; // get the middle index of the list after dividing it by 2 to split the list in half
@@ -10,14 +10,14 @@ public class Java_merge_sort { // Merge sort algorithm
             mergeSort(list.subList(0, middleIndex)); // get the left list 
         List<Integer> rightList =
             mergeSort(list.subList(middleIndex, list.size())); // get the right list
-        List<Integer> sortedList = new ArrayList<Integer>(); // create a new list
+        List<Integer> sortedList = new ArrayList<Integer>(); 
         int leftIndex = 0; 
         int rightIndex= 0; 
         int leftLength = leftList.size(); 
         int rightLength = rightList.size();
-        while (leftIndex < leftLength&& rightIndex < rightLength) { // while the left and right index are less than the length of the left and right list
+        while (leftIndex < leftLength&& rightIndex < rightLength) { 
             if (leftList.get(leftIndex) < rightList.get(rightIndex)) {
-                sortedList.add(leftList.get(leftIndex)); // add the left list to the sorted list when if condition is true
+                sortedList.add(leftList.get(leftIndex)); 
                 leftIndex++; // then increment the left index
             } else {
                 sortedList.add(rightList.get(rightIndex)); // add the right list to the sorted list when if condition is not true
@@ -26,7 +26,7 @@ public class Java_merge_sort { // Merge sort algorithm
         }
         sortedList.addAll(leftList.subList(leftIndex, leftLength)); 
         sortedList.addAll(rightList.subList(rightIndex, rightLength));
-        return sortedList; // return the sorted list
+        return sortedList; // get the sortedList
     }
 
     public static void main(String[] args) {
