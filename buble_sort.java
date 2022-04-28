e42a6427f051152d801dbc487d64b2b6abf24aed
GUENWWE <52918965+GUENWWE@users.noreply.github.com>
2022-04-28 21:13:50 +0700

    Java base on C++

---
+++ buble_sort.java
@@ -0,0 +1,33 @@
+public class buble_sort {
+
+  public static void bublesort(int arr[], int n) {
+    int temp;
+    for ( int i = 0; i < n - 1; i++) {
+      for ( int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    System.out.print("Sorted Array: ");
+    for (int i = 0; i < n; i++){// print the sorted array
+    System.out.print(arr[i] + " ");
+    }
+    
+    
+  }
+  public static void main (String[] args) {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = arr.length; // get the length of the array
+    bublesort(arr, n);
+/* 
+    System.out.print("Sorted Array: ");
+    for (int i = 0; i < n; i++)  // print the sorted array
+    System.out.print(arr[i] + " "); */
+    
+
+  } 
+  
+}
