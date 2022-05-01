3419e8fd4797349c77dbfe157b3b1cf8e2ba3904
GUENTIGER <52918965+GUENTIGER@users.noreply.github.com>
2022-05-01 20:55:58 +0700

    Update model.py

--- Python/model.py
+++ Python/model.py
@@ -12,7 +12,7 @@ class Linear_QNet(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
-        x = self.linear2( ưx)
+        x = self.linear2(x)
         return x
 
     def save(self, file_name='model.pth'):
@@ -66,5 +66,3 @@ class QTrainer:
         loss.backward()
 
         self.optimizer.step()
-
-
